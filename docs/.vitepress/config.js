4115a9ae2ec5a95f9c2b0c9682dae1c93bdc90cc
海胆 <8065350+seaurchin486@user.noreply.gitee.com>
2022-06-23 01:48:38 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  // base: '/life-experience-book/',
+  title: '海胆的生活冷知识',
+  description: '虽然很冷，但确实有用~',
+  themeConfig: {
+    logo: '/assets/img/move-bird.gif'
+  }
+})
